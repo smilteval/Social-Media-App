457d0b55bdac4daf00cf7119f0722ed59e0488c4
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-20 15:16:17 -0500

    Set up database and server

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const {SECRET_KEY} = require("../../config")
+const User = require("../../models/User")
+
+module.exports = {
+    Mutation:{
+        async register(_, 
+            {
+                registerInput: {username, email, password, confirmPassword}
+            },
+            context,
+            info
+        ) {
+            //Validate user data
+            //Make sure user doesn't already exist
+            
+            const user = User.findOne
+
+            //hash password
+            //create auth token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            })
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, {expiresIn: "1h"})
+
+            return{
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+    }
+}
