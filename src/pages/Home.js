96aeef2d1fb268761965d2caf8a5399f5638834c
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-21 15:31:00 -0500

    added create post form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
 
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 export default function Home() {
-  const {
-    loading,
-    data: { getPosts: posts } ={}
-  } = useQuery(FETCH_POSTS_QUERY);
+  const { user } = useContext(AuthContext);
+
+  const { loading, data: { getPosts: posts } = {} } = useQuery(
+    FETCH_POSTS_QUERY
+  );
 
   return (
     <Grid columns={3}>
@@ -17,6 +20,11 @@ export default function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
@@ -31,25 +39,3 @@ export default function Home() {
     </Grid>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
