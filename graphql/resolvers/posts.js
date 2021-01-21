c966d69e435a1d05dcb7dcf54472c2ff5207cee9
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-01-21 01:07:29 -0500

    implemented like/unlike

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
+
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
@@ -51,10 +52,29 @@ module.exports = {
         } else {
           throw new AuthenticationError("Action not allowed");
         }
-      }
-      catch(err){
+      } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
